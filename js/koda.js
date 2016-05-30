a9df6b42bcbe64c71b6381323a3c564df2c38c2e
Enej Ravbar <enej.ravbar@siol.net>
2016-05-30 21:04:24 +0200

    JavaScript - delna implementacija registracije novega uporabnika

--- js/koda.js
+++ js/koda.js
@@ -38,7 +38,133 @@ function generirajPodatke(stPacienta) {
   return ehrId;
 }
 
+function kreirajUporabnika(){
+
+   $("#gumb-registriraj").click(function(){
+      var ime=$("#registracija-ime").val();
+      var priimek=$("#registracija-priimek").val();
+      var datum=$("#registracija-datum").val();
+      var ura=$("#registracija-ura").val();
+      //console.log("preveriVeljavnostDatuma: " +datum+ " preveriVeljavnostUre: " + ura);
+      if(!preveriVeljavnostDatuma(datum)){ 
+        $("#registracija-datum").parent().attr({"class" : "input-group col-sm-10 has-error"});
+        return;
+      }else{
+        $("#registracija-datum").parent().attr({"class" : "input-group col-sm-10"});
+      }
+      if(!preveriVeljavnostUre(ura)){
+        $("#registracija-ura").parent().attr({"class" : "input-group col-sm-10 has-error"});
+        return;
+      }else{
+        $("#registracija-ura").parent().attr({"class" : "input-group col-sm-10"});
+      }
+
+      var datumRojstva= zapisiDatumRojstvaVFormatu(datum, ura);
+      console.log("datum rojstva: " + datumRojstva);
+    
+     /*$.ajaxSetup({
+            headers: {"Ehr-Session": sessionId}
+        });
+        $.ajax({
+            url: baseUrl + "/ehr",
+            type: 'POST',
+            success: function (data) {
+                var ehrId = data.ehrId;
+                var partyData = {
+                    firstNames: ime,
+                    lastNames: priimek,
+                    dateOfBirth: datumRojstva,
+                    partyAdditionalInfo: [{key: "ehrId", value: ehrId}]
+                };
+                $.ajax({
+                    url: baseUrl + "/demographics/party",
+                    type: 'POST',
+                    contentType: 'application/json',
+                    data: JSON.stringify(partyData),
+                    success: function (party) {
+                        if (party.action == 'CREATE') {
+                            console.log("Uspešno kreiran zapis " +ehrId);
+                        }
+                    },
+                    error: function(err) {
+                      
+                    }
+                });
+            }
+        });*/
+
+    });  
+}
+
+function preveriVeljavnostDatuma(datum){
+  var stVezajev=0;
+  for(var i=0; i<datum.length; i++){
+    //console.log("Trenutni znak "+ datum.charAt(i));
+    if(datum.charAt(i)=="-"){
+      stVezajev++;
+    }
+  }
+
+  var tabela =datum.split("-");
+  if(stVezajev==2){
+    for(var i=0; i<tabela.length; i++){
+      if(tabela[i]<10){
+        //console.log(tabela[i]);
+        if(tabela[i].charAt(0)!="0"){
+          //console.log("Tabela datumov ia na poziciji " +i +"nima ničle");
+          return false;
+        }
+      }
+    }
+  }
+
+  if(tabela.length=3 && tabela[0]>=1 && tabela[0]<=31 && tabela[1]>=1 && tabela[1]<=12 && tabela[2]>1950){  // format 12.4.2016
+
+    return true;
+  }
+  return false;
+}
+
+function preveriVeljavnostUre(ura){
+  var stDvopicij=0;
+  for(var i=0; i<ura.length; i++){
+    if(ura.charAt(i)==":"){
+      stDvopicij++;
+    }
+  }
+  var tabela =ura.split(":");
+  if(stDvopicij==1){
+    for(var i=0; i<tabela.length; i++){
+      if(tabela[i]<10){
+        if(tabela[i].charAt(0)!="0"){
+          return false;
+        }
+      }
+    }
+  }
+  
+
+  if(tabela.length==2 && tabela[0]>=0 && tabela[0]<=23 && tabela[1]>=0 && tabela[1]<=59){  // format 12.4.2016
+
+    return true;
+  }
+  return false;
+}
+
+function zapisiDatumRojstvaVFormatu(datum, ura){
+  var tabelaDatum = datum.split("-");
+  var format=tabelaDatum[2]+"-"+tabelaDatum[1]+"-"+tabelaDatum[0]+"T"+ura;
+  return format;
+}
+
 $(document).ready(function(){
+
+  kreirajUporabnika();
+
+  $("#preberiPredlogoBolnika").change(function(){
+    console.log("Izbran je bil: " + $("#preberiPredlogoBolnika option:selected" ).text());
+  });
+  
 
   $(".glyphicon").click(function(){
 
@@ -115,7 +241,7 @@ $(document).ready(function(){
           barva="alert alert-danger"; 
           break;
       }
-      console.log(kategorija + "  "+ barva);
+      //console.log(kategorija + "  "+ barva);
       $("#ITM-okvir").css({"display" : "block"});
       $("#ITM-okvir").attr({"class" : barva});
       $("#ITM-podatek").text(ITM.toFixed(1));
