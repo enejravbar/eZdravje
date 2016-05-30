0a277040b33df8697ee108578bbd83cc25640c40
Enej Ravbar <enej.ravbar@siol.net>
2016-05-30 18:10:13 +0200

    JavaScript - funkcionalnost ITM kalkulator

--- js/koda.js
+++ js/koda.js
@@ -38,5 +38,89 @@ function generirajPodatke(stPacienta) {
   return ehrId;
 }
 
+$(document).ready(function(){
 
-// TODO: Tukaj implementirate funkcionalnost, ki jo podpira vaša aplikacija
+  $(".glyphicon").click(function(){
+
+        if($(this).attr("class").indexOf("glyphicon-menu-down")>-1){
+          $(this).parent().parent().find(".panel-body").slideToggle("slow");
+          $(this).attr({
+            "class" : "glyphicon glyphicon-menu-up"
+          });
+        }else{
+          $(this).parent().parent().find(".panel-body").slideToggle("slow");
+          $(this).attr({
+            "class" : "glyphicon glyphicon-menu-down"
+          });
+        }
+  });
+
+  $("#gumbITM").click(function(){
+
+    var teza = $("#teza").val();
+    var visina = ($("#visina").val())/100;
+    var ITM;
+    var napaka1,napaka2;
+    var kategorija="";
+    var barva="";
+    napaka1=true;
+    napaka2=true;
+
+    if(teza>0){
+      napaka1=false;
+      $("#teza").parent().attr({"class" : "input-group"});
+    }else{
+      $("#teza").parent().attr({"class" : "input-group has-error"});
+    }
+    if(visina>0){
+      napaka2=false;
+      $("#visina").parent().attr({"class" : "input-group"});
+    }else{
+       $("#visina").parent().attr({"class" : "input-group has-error"});
+    }
+    if(!napaka1 && !napaka2){
+      console.log("Test1");
+      ITM=teza/(visina*visina);
+      switch(true){
+        case (ITM<=16): 
+          kategorija="HUDA PODHRANJENOST"; 
+          barva="alert alert-info";
+          break;
+        case (ITM>=16 && ITM<17): 
+          kategorija="ZMERNA PODHRANJENOST"; 
+          barva="alert alert-info";
+          break;
+        case (ITM>=17 && ITM<18.5):
+          kategorija="BLAGA PODHRANJENOST"; 
+          barva="alert alert-info";
+          break;
+        case (ITM>=18.5 && ITM<25):
+          kategorija="NORMALNA TELESNA MASA";
+          barva="alert alert-success"; 
+          break;
+        case (ITM>=25 && ITM<30):
+          kategorija="ZVEČANA TELESNA MASA";
+          barva="alert alert-warning";
+          break;
+        case (ITM>=30 && ITM<35):
+          kategorija="DEBELOST STOPNJE I"; 
+          barva="alert alert-danger";
+          break;
+        case (ITM>=35 && ITM<40):
+          kategorija="DEBELOST STOPNJE II";
+          barva="alert alert-danger"; 
+          break;
+        case (ITM>=40):
+          kategorija="DEBELOST STOPNJE III";
+          barva="alert alert-danger"; 
+          break;
+      }
+      console.log(kategorija + "  "+ barva);
+      $("#ITM-okvir").css({"display" : "block"});
+      $("#ITM-okvir").attr({"class" : barva});
+      $("#ITM-podatek").text(ITM.toFixed(1));
+      $("#ITM-kategorija").text(kategorija);
+    }
+  });
+
+});
